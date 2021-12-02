c66a9c2459e9e84dbabcbcb69492b61df4138c24
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2021-12-02 09:38:41 +0100

    Removed elixir project for now

--- lib/depseus.ex
+++
@@ -1,6 +0,0 @@
-defmodule Depseus do
-  @moduledoc """
-  Depseus is a tool to automatically generate GitHub issues in Elixir repos when it's time to update dependencies,
-  Elxir or Erlang.
-  """
-end
